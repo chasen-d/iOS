0be5042a6012706e08e53cbe4c67a74a714f334e
changcai <changcai@ios-developer.local>
2018-09-28 15:45:46 +0800

    2018.9.28基础搭建

---
+++ iOSProject/iOSProject/Module/HomePage/BasicKnowledge(基础知识)/CSBasicKnowledgeController.h
@@ -0,0 +1,17 @@
+//
+//  CSBasicKnowledgeController.h
+//  iOSProject
+//
+//  Created by changcai on 2018/9/28.
+//  Copyright © 2018年 changcai. All rights reserved.
+//
+
+#import "CSBaseTableViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CSBasicKnowledgeController : CSBaseTableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSProject/iOSProject/Module/HomePage/BasicKnowledge(基础知识)/CSBasicKnowledgeController.m
@@ -0,0 +1,65 @@
+//
+//  CSBasicKnowledgeController.m
+//  iOSProject
+//
+//  Created by changcai on 2018/9/28.
+//  Copyright © 2018年 changcai. All rights reserved.
+//
+
+#import "CSBasicKnowledgeController.h"
+
+@interface CSBasicKnowledgeController ()
+
+@end
+
+@implementation CSBasicKnowledgeController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.dataSoureArray = @[ @{@"title":@"列表加载动画",@"className":@"CSShowTableViewController"},
+                             ];
+    
+}
+
+#pragma mark - Table view data source
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+
+
+@end
